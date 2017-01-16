8eb23db556dbe8786af839e38d1ce4c2ad488eaa
Yoga Prakoso <yogaprakoso90@gmail.com>
2017-01-16 17:01:46 +0000

    add step1.js

---
+++ step1.js
@@ -0,0 +1,25 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks() {
+
+  var requestOptions = {
+   host: 'sytantris.github.io',
+   path: '/http-examples/step1.html'
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf-8');
+
+    response.on('data', function(data) {
+      console.log(data + "\n");
+    });
+
+    response.on('end', function() {
+      console.log("The end of chunk.");
+    });
+
+  });
+
+}
+
+getAndPrintHTMLChunks();
